12277e8b62d5de12c8ef2decc3d31b9d5fdffede
abdullaabdulraoof <abdullaabdulraoof66@gmail.com>
2025-09-02 23:51:59 +0530

    fix:cloudinary

--- Backend/cloudinaryConfig.js
+++ Backend/cloudinaryConfig.js
@@ -8,7 +8,7 @@ const multer = require("multer");
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: 154661966753662,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    api_secret: ewzkoqok0cQ1vVDAjsw4abg9Tmg,
 });
 console.log("Cloudinary Config:", {
     name: process.env.CLOUDINARY_CLOUD_NAME,
