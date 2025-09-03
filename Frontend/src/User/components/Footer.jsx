6d94cb83ec3a2d9419b93bff119b7d9f5e47066f
abdullaabdulraoof <abdullaabdulraoof66@gmail.com>
2025-09-03 19:56:58 +0530

    fix:flex 40

--- Frontend/src/User/components/Footer.jsx
+++ Frontend/src/User/components/Footer.jsx
@@ -37,8 +37,8 @@ export const Footer = () => {
                         </div>
                     </div>
                 </div>
-                <div className='flex'>
-                    <ul className='flex flex-row lg:flex-col gap-3 text-xs'>
+                <div className='flex justify-start'>
+                    <ul className='flex flex-col gap-3 text-xs'>
                         <li className='font-bold'>Products</li>
                         <li className='text-gray-400 hover:cursor-pointer hover:text-blue-500'>Headphones</li>
                         <li className='text-gray-400 hover:cursor-pointer hover:text-blue-500'>Smart Watches</li>
