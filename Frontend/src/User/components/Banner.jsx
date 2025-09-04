6891640ba3988d41de0f7a466dc889f336f14a9d
abdullaabdulraoof <abdullaabdulraoof66@gmail.com>
2025-09-04 11:36:55 +0530

    fix:grid in banner

--- Frontend/src/User/components/Banner.jsx
+++ Frontend/src/User/components/Banner.jsx
@@ -13,13 +13,13 @@ export const Banner = () => {
 
                 <div className='flex flex-wrap gap-10 overflow-hidden h-fit justify-center items-center myContainer mx-auto px-4 md:px-10 lg:px-60 py-2 '>
 
-                    <div className="grid grid-cols-6 md:grid-cols-3 gap-6 w-full h-[300px]">
+                    <div className="grid grid-cols-3  md:grid-cols-6 gap-6 w-full h-[300px]">
 
 
-                        <div className="col-span-6 md:col-span-3 relative rounded-xl bg-cover bg-center bg-[url('/banner/s22.png')] transform translate-all duration-500 ease-in-out hover:shadow-[0_0_12px_#5694F7] hover:scale-x-105">
+                        <div className="col-span-3 md:col-span-6 relative rounded-xl bg-cover bg-center bg-[url('/banner/s22.png')] transform translate-all duration-500 ease-in-out hover:shadow-[0_0_12px_#5694F7] hover:scale-x-105">
                         </div>
 
-                        <div className="col-span-6 md:col-span-3 relative rounded-xl bg-cover bg-center bg-[url('/banner/ipad.png')] transform translate-all duration-500 ease-in-out hover:shadow-[0_0_12px_#5694F7] hover:scale-x-105">
+                        <div className="col-span-3 md:col-span-6 relative rounded-xl bg-cover bg-center bg-[url('/banner/ipad.png')] transform translate-all duration-500 ease-in-out hover:shadow-[0_0_12px_#5694F7] hover:scale-x-105">
                         </div>
 
 
