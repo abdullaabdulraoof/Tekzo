50cbad751f80f2085d63c557f67b3af02e8c8454
abdullaabdulraoof <abdullaabdulraoof66@gmail.com>
2025-09-04 11:42:16 +0530

    fix: grid in Banner

--- Frontend/src/User/components/Banner.jsx
+++ Frontend/src/User/components/Banner.jsx
@@ -13,7 +13,7 @@ export const Banner = () => {
 
                 <div className='flex flex-wrap gap-10 overflow-hidden h-fit justify-center items-center myContainer mx-auto px-4 md:px-10 lg:px-60 py-2 '>
 
-                    <div className="grid grid-cols-6  md:grid-cols-6 gap-6 w-full h-[300px]">
+                    <div className="grid grid-cols-6 gap-6 w-full h-[300px]">
 
 
                         <div className="col-span-6 md:col-span-3 relative rounded-xl bg-cover bg-center bg-[url('/banner/s22.png')] transform translate-all duration-500 ease-in-out hover:shadow-[0_0_12px_#5694F7] hover:scale-x-105">
