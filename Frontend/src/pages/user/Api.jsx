e9414f67b921922fa30679c112c941e930283db0
abdullaabdulraoof <abdullaabdulraoof66@gmail.com>
2025-09-10 19:58:11 +0530

    fix:gmail logging

--- Frontend/src/pages/user/Api.jsx
+++ Frontend/src/pages/user/Api.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:'https://tekzo.onrender.com/api/auth'
+    baseURL:'https://tekzo.onrender.com/api'
 })
 export const googleAuth = (code) => api.get(`/google?code=${code}`)
