cfe680b763d48d808b9f2ce55df7ffc709a1f74e
abdullaabdulraoof <abdullaabdulraoof66@gmail.com>
2025-09-10 22:17:29 +0530

    fix:gmail logging

--- Backend/model/user.js
+++ Backend/model/user.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String},
     role: { type: String, enum: ["user", "admin"], default: "user" }, // ✅ Add role
     addresses :[{
         address:String,
