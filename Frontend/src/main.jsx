02293cdcb6c39031f81024abd6a08bf9a5682da5
abdullaabdulraoof <abdullaabdulraoof66@gmail.com>
2025-09-11 20:43:27 +0530

    fix:authentication

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,19 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { CartProvider } from '../context/CartContext.jsx'
-import { GoogleOAuthProvider } from '@react-oauth/google'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { CartProvider } from "../context/CartContext.jsx";
+import { AuthProvider } from "../context/AuthContext.jsx";   // ✅ import
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId='462576015706-r4o1bb51h6luatb39goilu921jbj3uvl.apps.googleusercontent.com'>
-      <CartProvider>
-        <Router>
-          <App />
-        </Router>
-      </CartProvider>
+    <GoogleOAuthProvider clientId="462576015706-r4o1bb51h6luatb39goilu921jbj3uvl.apps.googleusercontent.com">
+      <AuthProvider>   {/* ✅ wrap here */}
+        <CartProvider>
+          <Router>
+            <App />
+          </Router>
+        </CartProvider>
+      </AuthProvider>
     </GoogleOAuthProvider>
   </StrictMode>
-)
+);
