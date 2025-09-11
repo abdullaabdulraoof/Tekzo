cfeb498f5de53351e3f82023541d4b143df334d7
abdullaabdulraoof <abdullaabdulraoof66@gmail.com>
2025-09-11 10:44:06 +0530

    fix:login page

--- Frontend/src/User/features/auth/Login.jsx
+++ Frontend/src/User/features/auth/Login.jsx
@@ -106,7 +106,7 @@ export const Login = () => {
                                 Login
                             </button>
                         </form>
-                        
+
                         <div className="flex items-center gap-2 my-5">
                             <div className="flex-grow h-px bg-gray-600"></div>
                             <span className="text-xs text-gray-400">OR</span>
@@ -116,7 +116,7 @@ export const Login = () => {
                       
                         <button
                             onClick={googleLogin}
-                            className="flex items-center justify-center gap-2 w-full bg-white text-gray-800 py-2 rounded-lg text-sm font-medium shadow-md hover:shadow-lg transition"
+                            className="flex items-center justify-center gap-2 w-full bg-white text-gray-800 py-2 rounded-lg text-sm font-medium transform transition-all duration-500 ease-in-out hover:shadow-[0_0_12px_#5694F7] hover:scale-x-105"
                         >
                             <img
                                 src="https://www.svgrepo.com/show/475656/google-color.svg"
