678d8ff6ee5721c8fd59937d4ceb6a3d093b349c
abdullaabdulraoof <abdullaabdulraoof66@gmail.com>
2025-09-12 18:22:30 +0530

    fix:jwtdecode

--- Frontend/src/utils/checkToken.jsx
+++ Frontend/src/utils/checkToken.jsx
@@ -1,13 +1,12 @@
 // utils/checkToken.js
-import { jwtDecode } from "jwt-decode";  // ✅ correct for v4+
-
+import jwtDecode from "jwt-decode";  // ✅ works with v3.1.2
 
 export const isTokenValid = () => {
     const token = localStorage.getItem("userToken");
     if (!token) return false;
 
     try {
-        const decoded = jwtDecode(token); // ✅
+        const decoded = jwtDecode(token);
         const currentTime = Date.now() / 1000;
         return decoded.exp > currentTime;
     } catch (err) {
