842b48bcc6c1b03d8bc77197b6f4cd81b5d9bcdb
abdullaabdulraoof <abdullaabdulraoof66@gmail.com>
2025-09-12 18:43:01 +0530

    fix: useEffect in orderSuccess

--- Frontend/src/User/features/Order Success/OrderSuccess.jsx
+++ Frontend/src/User/features/Order Success/OrderSuccess.jsx
@@ -1,5 +1,5 @@
     import React from 'react'
-    import { useNavigate } from 'react-router-dom'
+    import { useNavigate,useEffect } from 'react-router-dom'
 
     export const OrderSuccess = () => {
         const token = localStorage.getItem("userToken")
