623eb485922a00b91211820b0010d854a98914f2
abdullaabdulraoof <abdullaabdulraoof66@gmail.com>
2025-09-12 09:31:13 +0530

    fix:pass in userSchema

--- Backend/model/user.js
+++ Backend/model/user.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: false },
     role: { type: String, enum: ["user", "admin"], default: "user" }, // ✅ Add role
     addresses :[{
         address:String,
