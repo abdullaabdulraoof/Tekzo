d64b7af0f450db92104603731f7da9170297844c
abdullaabdulraoof <abdullaabdulraoof66@gmail.com>
2025-10-15 00:39:25 +0530

    fix: import in Profilee page

--- Frontend/src/pages/user/Account/ProfileePage.jsx
+++ Frontend/src/pages/user/Account/ProfileePage.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Profilee } from '../../../User/features/Account/Profilee'
+import { Navbar } from '../../../User/components/Navbar'
+import { Footer } from '../../../User/components/Footer'
 
 export const ProfileePage = () => {
     return (
