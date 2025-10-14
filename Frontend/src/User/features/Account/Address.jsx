1ca6f271e2f68388561c68b75b1e636090342b66
abdullaabdulraoof <abdullaabdulraoof66@gmail.com>
2025-10-15 01:22:29 +0530

    fix: Address

--- Frontend/src/User/features/Account/Address.jsx
+++ Frontend/src/User/features/Account/Address.jsx
@@ -22,7 +22,7 @@ export const Address = () => {
         }
     }, [token, navigate]);
 
-    
+
 
     useEffect(() => {
         const fetchAccount = async () => {
@@ -32,7 +32,7 @@ export const Address = () => {
                     { headers: { Authorization: `Bearer ${token}` }, withCredentials: true }
                 );
 
-                
+
 
                 if (res.data.defaultAddress) {
                     setDefaultAddress(res.data.defaultAddress);
@@ -58,7 +58,7 @@ export const Address = () => {
                 { address, pincode, country }, // ✅ body
                 { headers: { Authorization: `Bearer ${token}` } } // ✅ config
             );
-            
+
             setDefaultAddress(res.data.defaultAddress);
         } catch (err) {
             console.error("Error updating address:", err);
@@ -66,59 +66,59 @@ export const Address = () => {
     };
     return (
 
-                    
 
 
-                        <div className='flex flex-col justify-start items-center border border-gray-700/70 p-6 rounded-xl w-[40%] h-[400px] gap-4'>
 
-                            <h1 className='text-xl font-bold '>
-                                Default Address
-                            </h1>
-                            {defaultAddress ?
-                                (
-                                    <div className='flex gap-4 w-full justify-between'>
-                                        <div className='flex gap-3 text-gray-400 text-sm pt-2 '>
-                                            <div className='flex gap-2'>
+        <div className='flex flex-col justify-start items-center border border-gray-700/70 p-6 rounded-xl w-full h-[400px] gap-4'>
 
-                                                <label>Address : </label>
-                                                <input className="rounded-xl px-2 py-2 text-sm bg-black border border-gray-400/20 outline-none" type="text" value={address} onChange={(e) => {
-                                                    setAddress(e.target.value)
-                                                }} />
-                                            </div>
-                                            <div className='flex gap-2'>
+            <h1 className='text-xl font-bold '>
+                Default Address
+            </h1>
+            {defaultAddress ?
+                (
+                    <div className='flex flex-col gap-4 w-full justify-between'>
+                        <div className='flex gap-3 text-gray-400 text-sm pt-2 '>
+                            <div className='flex justify-center items-center gap-2 w-1/2'>
 
-                                                <label>Pincode : </label>
-                                                <input className="rounded-xl px-2 py-2 text-sm bg-black border border-gray-400/20 outline-none" type="text" value={pincode} onChange={(e) => {
-                                                    setPincode(e.target.value)
-                                                }} />
-                                            </div>
+                                <label>Address: </label>
+                                <input className="rounded-xl px-2 py-2 text-sm bg-black border border-gray-400/20 outline-none w-full" type="text" value={address} onChange={(e) => {
+                                    setAddress(e.target.value)
+                                }} />
+                            </div>
+                            <div className='flex justify-center items-center gap-2 w-1/2'>
 
-                                            <div className='flex gap-2'>
+                                <label>Pincode : </label>
+                                <input className="rounded-xl px-2 py-2 text-sm bg-black border border-gray-400/20 outline-none  w-full" type="text" value={pincode} onChange={(e) => {
+                                    setPincode(e.target.value)
+                                }} />
+                            </div>
 
-                                                <label>country : </label>
-                                                <input className="rounded-xl px-2 py-2 text-sm bg-black border border-gray-400/20 outline-none" type="text" value={country} onChange={(e) => {
-                                                    setCountry(e.target.value)
-                                                }} />
-                                            </div>
-                                        </div>
-                                        <div >
+                            <div className='flex justify-start items-center gap-2 w-full'>
 
-                                            <button
-                                                className='flex justify-center items-center w-full bg-[#5694F7] py-2 px-3 rounded-xl text-xs gap-3 cursor-pointer text-white transform transition-all duration-500 ease-in-out hover:shadow-[0_0_12px_#5694F7] hover:scale-x-105' onClick={handleSubmit}
+                                <label>country : </label>
+                                <input className="rounded-xl px-2 py-2 text-sm bg-black border border-gray-400/20 outline-none w-full" type="text" value={country} onChange={(e) => {
+                                    setCountry(e.target.value)
+                                }} />
+                            </div>
+                        </div>
+                        <div className='flex justify-center items-center'>
 
-                                            >
-                                                <span className='font-bold'>Save Changes</span>
-                                            </button>
-                                        </div>
+                            <button
+                                className='flex justify-center items-center w-full bg-[#5694F7] py-2 px-3 rounded-xl text-xs gap-3 cursor-pointer text-white transform transition-all duration-500 ease-in-out hover:shadow-[0_0_12px_#5694F7] hover:scale-x-105' onClick={handleSubmit}
 
-
-                                    </div>
-
-                                ) : (<p className='text-gray-400'>Loading account details...</p>)}
+                            >
+                                <span className='font-bold'>Save Changes</span>
+                            </button>
                         </div>
 
 
-               
+                    </div>
+
+                ) : (<p className='text-gray-400'>Loading account details...</p>)}
+        </div>
+
+
+
     )
 }
 
